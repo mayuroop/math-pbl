05b22ef413099d6b5b6c22271acc3f75592e7a06
Mayur Kshirsagar <140840441+mayuroop@users.noreply.github.com>
2024-04-18 12:23:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import math
 
 app = Flask(__name__)
@@ -23,13 +23,6 @@ def distance_between_points(point1, point2):
     squared_distance = sum((a - b)**2 for a, b in zip(point1, point2))
     return math.sqrt(squared_distance)
 
-# Function to calculate the orthogonal projection of one vector onto another
-def orthogonal_projection(vector1, vector2):
-    dot_product_val = dot_product(vector1, vector2)
-    length2_squared = vector_length(vector2) ** 2
-    projection_scalar = dot_product_val / length2_squared
-    return [projection_scalar * x for x in vector2]
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -41,12 +34,16 @@ def calculate():
     point_1 = [int(x) for x in request.form['point_1'].split(',')]
     point_2 = [int(x) for x in request.form['point_2'].split(',')]
 
+ 
     length_vector_1 = vector_length(vector_1)
-    angle_vectors = vector_angle(vector_1, vector_2)
-    distance_points = distance_between_points(point_1, point_2)
-    projection_vector = orthogonal_projection(vector_1, vector_2)
+    
 
-    return render_template('result.html', length_vector_1=length_vector_1, angle_vectors=angle_vectors, distance_points=distance_points, projection_vector=projection_vector)
+    angle_vectors = vector_angle(vector_1, vector_2)
+    
+    
+    distance_points = distance_between_points(point_1, point_2)
+
+    return render_template('result.html', length_vector_1=length_vector_1, angle_vectors=angle_vectors, distance_points=distance_points)
 
 if __name__ == '__main__':
     app.run(debug=True)
